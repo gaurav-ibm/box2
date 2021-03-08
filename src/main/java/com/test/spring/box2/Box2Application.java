a8e9cf5a4583067ec81702630d86f547c1ed8cb4
Gaurav Verma <gaverma7@in.ibm.com>
2021-03-08 15:08:47 +0530

    first commit

---
+++ src/main/java/com/test/spring/box2/Box2Application.java
@@ -0,0 +1,13 @@
+package com.test.spring.box2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Box2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Box2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/spring/box2/Box2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.spring.box2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Box2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
